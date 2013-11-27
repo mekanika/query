11183750eb605dd0546b663838c9abbd1265fe60
Clint Walker <clint@neonrelic.com>
2013-11-27 10:44:43 +0800

    Update sorting and add support for multiple sort objects

--- test/sort.test.js
+++ test/sort.test.js
@@ -7,20 +7,60 @@ describe('Sorting methods', function() {
     query.reset();
   });
 
-  it('sets .asc( index )', function() {
-    expect( query().asc().paging.sort[0].direction ).to.be( 'asc' );
-    expect( query().asc('smoo').paging.sort[0].index ).to.be( 'smoo' );
+
+  describe('.asc( index )', function() {
+
+    it('adds an ascending sort object', function() {
+      expect( query().asc().sort[0].direction ).to.be( 'asc' );
+      expect( query().asc('smoo').sort[0].index ).to.be( 'smoo' );
+    });
+
+    it('optionally uses index', function() {
+      expect( query().asc().sort ).to.not.have.keys( 'index' );
+    });
   });
 
-  it('sets .desc( index )', function() {
-    expect( query().desc().paging.sort[0].direction ).to.be( 'desc' );
-    expect( query().desc('smoo').paging.sort[0].index ).to.be( 'smoo' );
+
+  describe('.desc( index )', function() {
+
+    it('adds a descending sort object', function() {
+      expect( query().desc().sort[0].direction ).to.be( 'desc' );
+      expect( query().desc('smoo').sort[0].index ).to.be( 'smoo' );
+    });
+
+    it('optionally uses index', function() {
+      expect( query().desc().sort ).to.not.have.keys( 'index' );
+    });
+
   });
 
-  it('sets .order( index ) by', function() {
-    expect( query().order('meep').paging.sort[0].index ).to.be('meep');
+
+  describe('.order( index )', function() {
+
+    it('sets the most recent sort object index', function() {
+      var q = query().asc();
+      expect( q.sort[0] ).to.not.have.keys( 'index' );
+      q.order('name');
+      expect( q.sort[0].index ).to.be( 'name' );
+    });
+
+    it('creates a sort object with `index` if none existing', function() {
+      var q = query();
+      expect( q.sort ).to.have.length( 0 );
+      expect( q.order('cool').sort ).to.have.length( 1 );
+      expect( q.sort[0] ).to.not.have.keys( 'direction' );
+    })
+
   });
 
-  it('supports multiple support indeces');
+
+  describe('multiple indeces', function() {
+
+    it('methods should add to sort queue', function() {
+      var q = query().asc('name').desc('price');
+      expect( q.sort ).to.have.length( 2 );
+    });
+
+  });
 
 });
