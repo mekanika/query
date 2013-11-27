2b948ae12b9065327a0b760bf234557a006e1740
Clint Walker <clint@neonrelic.com>
2013-11-27 10:55:34 +0800

    Update 'paging' -> 'display'

--- test/display.test.js
+++ test/display.test.js
@@ -1,7 +1,7 @@
 var query = require('../lib/index.js');
 var expect = require('expect.js');
 
-describe('Paging methods', function() {
+describe('Display methods', function() {
 
   beforeEach( function(){
     query.reset();
@@ -9,32 +9,32 @@ describe('Paging methods', function() {
 
   it('sets .limit(num)', function() {
     var q = query().limit(10);
-    expect( q.paging.limit ).to.be( 10 );
+    expect( q.display.limit ).to.be( 10 );
   });
 
   it('sets .offset(num)', function() {
     var q = query().offset(250);
-    expect( q.paging.offset ).to.be( 250 );
+    expect( q.display.offset ).to.be( 250 );
   });
 
   it('aliases .offset() as .skip()', function() {
     var q = query().skip(150);
-    expect( q.paging.offset ).to.be( 150 );
+    expect( q.display.offset ).to.be( 150 );
   });
 
   it('supports offset via .page(num)', function() {
 
     // Page 1 should return record 1 (ie. offset 0)
     var q = query().limit(10).page(1);
-    expect( q.paging.offset ).to.be( 0 );
+    expect( q.display.offset ).to.be( 0 );
 
     // Page 3 should return first result of 21 (if limit 10)
     // ie. Page 1 returns = 1-10
     //     Page 2 returns = 11-20
     //     Page 3 returns = 21-30 **(ie. offset 20)**
     q = query().limit(10).page(3);
-    expect( q.paging.limit ).to.be( 10 );
-    expect( q.paging.offset ).to.be( 20 );
+    expect( q.display.limit ).to.be( 10 );
+    expect( q.display.offset ).to.be( 20 );
   });
 
   it('fails to set page if limit not set first', function() {
